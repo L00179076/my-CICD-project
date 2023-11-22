a160785303e15e9ae5e3d9153f2d34dead693e2f
L00179076 <L00179076@atu.ie>
2023-11-22 10:59:39 +0000

    delete deploy code and making some changes in code

--- src/main/java/com/javaproject/githubcicdactions/GithubCicdActionsApplication.java
+++ src/main/java/com/javaproject/githubcicdactions/GithubCicdActionsApplication.java
@@ -12,7 +12,7 @@ public class GithubCicdActionsApplication {
 	   @GetMapping("/welcome")
        public String welcome()
 	   {
-		return "welcome ci/cd pipeline";
+		return "welcome";
 	   }
 	public static void main(String[] args) {
 		SpringApplication.run(GithubCicdActionsApplication.class, args);
