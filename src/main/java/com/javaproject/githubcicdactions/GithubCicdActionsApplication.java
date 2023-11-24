a41eaf5320eb3ad1ca6dd470e68803e7c832d1cb
L00179076 <L00179076@atu.ie>
2023-11-24 05:35:33 +0000

    made some changes in my code to commit

--- src/main/java/com/javaproject/githubcicdactions/GithubCicdActionsApplication.java
+++ src/main/java/com/javaproject/githubcicdactions/GithubCicdActionsApplication.java
@@ -12,7 +12,7 @@ public class GithubCicdActionsApplication {
 	   @GetMapping("/welcome")
        public String welcome()
 	   {
-		return "welcome to CI/CD pipeline";
+		return "welcome to CI/CD demo project";
 	   }
 	public static void main(String[] args) {
 		SpringApplication.run(GithubCicdActionsApplication.class, args);
